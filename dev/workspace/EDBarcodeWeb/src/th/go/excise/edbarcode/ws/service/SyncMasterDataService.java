220e74763313f498ec1b54bdd56354dc672adc98
taechapon <taechapon@gmail.com>
2015-07-21 17:55:18 +0700

    restructure project

--- dev/workspace/EDBarcodeWeb/src/th/go/excise/edbarcode/ws/service/SyncMasterDataService.java
+++
@@ -1,9 +0,0 @@
-package th.go.excise.edbarcode.ws.service;
-
-import th.go.excise.edbarcode.ws.oxm.Entrepreneur;
-
-public interface SyncMasterDataService {
-	
-	public Entrepreneur getMasterData(String licenseNo);
-	
-}
