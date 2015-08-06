b9813b3efe7193363291ad1665a990cdd05fcb2a
taechapon <taechapon@gmail.com>
2015-08-06 10:11:19 +0700

    Update test class

--- dev/workspace/EDBarcodeBack/src/test/java/th/go/excise/edbarcode/ws/test/MyTest.java
+++ dev/workspace/EDBarcodeBack/src/test/java/th/go/excise/edbarcode/ws/test/MyTest.java
@@ -8,14 +8,14 @@ public class MyTest {
 	
 	public static void main(String[] args) throws Exception {
 		
-		Company cCompany = new Company();
-		th.go.excise.edbarcode.ws.provider.oxm.Company pCompany = new th.go.excise.edbarcode.ws.provider.oxm.Company();
-		
-		
-		BeanUtils.copyProperties(cCompany, pCompany);
-		
-		System.out.println(cCompany);
-		System.out.println(pCompany);
+//		Company cCompany = new Company();
+//		th.go.excise.edbarcode.ws.provider.oxm.Company pCompany = new th.go.excise.edbarcode.ws.provider.oxm.Company();
+//		
+//		
+//		BeanUtils.copyProperties(cCompany, pCompany);
+//		
+//		System.out.println(cCompany);
+//		System.out.println(pCompany);
 	}
 	
 }
