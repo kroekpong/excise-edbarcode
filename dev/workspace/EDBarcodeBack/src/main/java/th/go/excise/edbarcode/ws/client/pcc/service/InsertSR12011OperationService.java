84e2bc5777dc69ce76f7002b4991e3fc0c5945e7
taechapon <taechapon@gmail.com>
2015-08-12 19:12:35 +0700

    Update field in pccservice.xsd

--- dev/workspace/EDBarcodeBack/src/main/java/th/go/excise/edbarcode/ws/client/pcc/service/InsertSR12011OperationService.java
+++ dev/workspace/EDBarcodeBack/src/main/java/th/go/excise/edbarcode/ws/client/pcc/service/InsertSR12011OperationService.java
@@ -1,5 +1,10 @@
 package th.go.excise.edbarcode.ws.client.pcc.service;
 
-public interface InsertSR12011OperationService {
+import th.go.excise.edbarcode.ws.client.pcc.oxm.InsertSR12011OperationRequest;
+import th.go.excise.edbarcode.ws.client.pcc.oxm.InsertSR12011OperationResponse;
 
+public interface InsertSR12011OperationService {
+	
+	public InsertSR12011OperationResponse doService(InsertSR12011OperationRequest request);
+	
 }
