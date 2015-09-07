0b65bf358a0dcae32173e3806f2f58ad9e75f38e
Meow Dev <arm2017@gmail.com>
2015-09-07 20:30:19 +0700

    add convert date

--- dev/workspace/ED_BARCODE_APP/WebContent/template/controller/profile-view-controller.js
+++ dev/workspace/ED_BARCODE_APP/WebContent/template/controller/profile-view-controller.js
@@ -12,14 +12,36 @@ module.service('$localProfileService', function() {
 module.controller('profile.view.controller', function($scope, $rootScope, $mdDialog, $location, $profileService, $localProfileService) {
 	console.info("profile.view.controller");
 
-	$scope.profile = $localProfileService.profile = $profileService.getProfile();
+	$scope.profile = $profileService.getProfile();
+	$localProfileService.profile = $profileService.getProfile();
 	$scope.factoryList = $localProfileService.factoryList;
 
+	
+	/**
+	 * convertThai
+	 * */
+	
+	function convertDateThai( myDate ){
+		
+		 var spDate = myDate.split("-");
+		 var date1 = spDate[0].substr(0,4);
+		 var date2 = spDate[1].substr(0,4);
+		 
+		 var newDate1 = (parseInt(date1) + 543).toString() + spDate[0].substr(4);
+		 var newDate2 = (parseInt(date2) + 543).toString() + spDate[1].substr(4);
+		 
+//		 console.log(newDate1,newDate2 );
+		 
+		 return newDate1 + "-" + newDate2;
+	}
+	
+	$scope.profile.convertDateThai = convertDateThai($scope.profile.LicenseDate);
+	
 	$scope.btnOrderClick = function() {
 		$location.path("/view/assignment");
 		$rootScope.$broadcast("updateTitle", 1);
 	};
-	
+
 	$scope.quickMenuClick = function(_index) {
 		$rootScope.$broadcast("gotoMenuIndex", _index);
 	};
@@ -53,10 +75,10 @@ module.controller('profile.view.controller', function($scope, $rootScope, $mdDia
 			if (answer == "OK") {
 				localStorage["LicenseNo"] = $scope.LicenseNo;
 				console.log("localStorage[\"LicenseNo\"] ", localStorage["LicenseNo"]);
-				
-//				goto loading
+
+				// goto loading
 				setTimeout(function() {
-					window.location="loading.html";
+					window.location = "loading.html";
 				}, 1000);
 			}
 			$mdDialog.hide(answer);
@@ -70,6 +92,9 @@ module.controller('profile.view.controller', function($scope, $rootScope, $mdDia
 			// console.log(_LicenseNo);
 			$scope.LicenseNo = _LicenseNo;
 		};
+		
+		$scope.convertDateThaifn = convertDateThai;
+//		console.log("fn",$scope.convertDateThaifn);
 
 	}
 
